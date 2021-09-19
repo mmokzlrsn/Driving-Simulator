1c3d147f6a07c9022edabaefb554f07cc64ea6b4
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-19 09:15:12 +0300

    Driving Simulator Project
    Completed Lab 1 Project of Unity Learn

---
+++ Driving Simulator/Driving Simualtor CWC/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 25.0f;
+    private float turnSpeed = 35.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //getting input from user
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // moving vehicle forward and backward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // rotating car but in order to do that car needs to be going backward or forward
+        if (forwardInput != 0)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
+    }
+}
